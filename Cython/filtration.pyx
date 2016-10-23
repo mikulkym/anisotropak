63824df8974c32676e06804f0e0e45f92722072f
mikulkym <mikulkym@gmail.com>
2016-10-23 20:12:10 +0200

    cython optimization

--- Cython/filtration.pyx
+++ Cython/filtration.pyx
@@ -1,7 +1,14 @@
-import math
+#cython: boundscheck=False, wraparound=False, nonecheck=False, cdivision=True
+# import math
 import copy
 import time
 from PIL import Image
+from libc.math cimport exp as c_exp
+from cpython cimport array
+
+#template pole
+cdef array.array float_array_template = array.array('f', [])
+cdef array.array int_array_template = array.array('i', [])
 
 
 def greyScale(img, int h, int w):
@@ -18,7 +25,7 @@ def greyScale(img, int h, int w):
     return greyPicture
 
 
-def anisotropie(img, int h, int w, float lambdaValue=0.1, float sigma=0.015):
+def anisotropie(float[:] img, int h, int w, float lambdaValue=0.1, float sigma=0.015):
     """
     Anisotropic filtering (AF), a method of enhancing the image quality of textures
     :param img: greyScale image
@@ -28,16 +35,28 @@ def anisotropie(img, int h, int w, float lambdaValue=0.1, float sigma=0.015):
     :param sigma:
     :return: filtered picture
     """
-    # h, w = shape
 
-    newPicture = copy.copy(img)
+    cdef float north = 0.0, south = 0.0, west = 0.0, east = 0.0, actual = 0.0
+    cdef float northGradient = 0.0, southGradient = 0.0, westGradient = 0.0, eastGradient = 0.0
+    cdef float cN, cS, cW, cE
+    cdef int nt
+    cdef int N = h - 2, M = w - 2
+    cdef int column, row, r, c
 
-    for row in range(1, h - 1):
-        for column in range(1, w - 1):
-            north = float(img[(row - 1) * w + column])
-            south = float(img[(row + 1) * w + column])
-            west = float(img[row * w + (column - 1)])
-            east = float(img[row * w + (column + 1)])
+    # vytvarim pole
+    cdef array.array newPic = array.clone(float_array_template, (h * w), zero=False)
+    # reference na pole, pointer
+    cdef float[:] newPicture = newPic
+
+    for r in range(N):
+        row = r + 1
+        for c in range(M):
+            column = c + 1
+
+            north = (img[(row - 1) * w + column])
+            south = (img[(row + 1) * w + column])
+            west = (img[row * w + (column - 1)])
+            east = (img[row * w + (column + 1)])
 
             actual = float(img[row * w + column])
 
@@ -46,10 +65,10 @@ def anisotropie(img, int h, int w, float lambdaValue=0.1, float sigma=0.015):
             westGradient = west - actual
             eastGradient = east - actual
 
-            cN = math.exp(-((northGradient ** 2) / (sigma ** 2)))
-            cS = math.exp(-((southGradient ** 2) / (sigma ** 2)))
-            cW = math.exp(-((westGradient ** 2) / (sigma ** 2)))
-            cE = math.exp(-((eastGradient ** 2) / (sigma ** 2)))
+            cN = c_exp(-((northGradient ** 2) / (sigma ** 2)))
+            cS = c_exp(-((southGradient ** 2) / (sigma ** 2)))
+            cW = c_exp(-((westGradient ** 2) / (sigma ** 2)))
+            cE = c_exp(-((eastGradient ** 2) / (sigma ** 2)))
 
             newPicture[row * w + column] = actual * (1 - lambdaValue * (cN + cS + cW + cE)) + lambdaValue * (
             cN * north + cS * south + cW * west + cE * east)
@@ -65,7 +84,7 @@ cdef void showPicture(picture, int h, int w):
     :param w: width of picture
     """
 
-    img = Image.new('L', h, w)
+    img = Image.new('L', (h, w))
     # h, w = shape
     for x in range(w):
         for y in range(h):
@@ -74,7 +93,7 @@ cdef void showPicture(picture, int h, int w):
     img.rotate(270).transpose(Image.FLIP_LEFT_RIGHT).show()
 
 
-def main():
+def main(int count=10):
     img = Image.open('../lena.png')
     shape = img.size
     obrazek = img.getdata()
@@ -86,18 +105,21 @@ def main():
     showPicture(greyPicture, h, w)
 
     # generator dela rovnou nove pole. takze kopie
-    aniPicture = [greyPicture[i] / 255.0 for i in range(h * w)]
+    aniPictureTmp = [greyPicture[i] / 255.0 for i in range(h * w)]
+    cdef array.array aniPictureArr = array.array('f', aniPictureTmp)
+    cdef float[:] aniPicture = aniPictureArr
 
     start = time.time()
-    for i in range(0, 10):
+    for i in range(count):
         aniPicture = anisotropie(aniPicture, h, w)
-        print i
+        # print i
     end = time.time()
     anisotropic_time = end - start
     print 'Anisotropic filtration time {0}'.format(anisotropic_time)
 
-    aniPicture = [int(aniPicture[i] * 255.0) for i in range(h * w)]
-    showPicture(aniPicture, h, w)
+    aniPictureNormal = [int(aniPicture[i] * 255.0) for i in range(h * w)]
+    showPicture(aniPictureNormal, h, w)
+    # Anisotropic filtration time 0.164086103439
 
 
 if __name__ == "__main__":
--- Cython/setup.py
+++ Cython/setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-# from distutils.extension import Extension
+from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
+
+ext_modules=[ Extension("filtration",
+              ["filtration.pyx"],
+              libraries=["m"],
+              extra_compile_args = ["-ffast-math"])]
 setup(
 
     cmdclass = {'build_ext': build_ext},
-    # ext_modules = [Extension("gaussian", ["gaussian.pyx", "utils.pyx"])]
-    ext_modules = cythonize("filtration.pyx")
+    ext_modules = ext_modules
+    # ext_modules = cythonize("filtration.pyx")
 )
